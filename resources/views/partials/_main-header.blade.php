1a245d9d88a55066b78724e0b3d4374500767d4a
gbelot2003 <gbelot2003@hotmail.com>
2015-09-10 11:10:46 -0600

    Permisos de visibilidad en menu profile

--- resources/views/partials/_main-header.blade.php
+++ resources/views/partials/_main-header.blade.php
@@ -12,11 +12,18 @@
 	<!-- Profile -->
 	<ul class="nav nav-list pull-right">
 		<li>
+			@if (! Auth::check())
+				<a data-toggle="menu" href="#profile">
+					<span class="access-hide">login</span>
+					<span class="icon icon-3x">account_box</span>
+				</a>
+			@else
 			<a data-toggle="menu" href="#profile">
-				<span class="access-hide">John Smith</span>
+				<span class="access-hide">{{ Auth::user()->name }}</span>
 				<span class="icon icon-3x">face</span>
 			</a>
 		</li>
+		@endif
 	</ul>
 	<!-- Profile -->
 
--- resources/views/partials/_profile-nav.blade.php
+++ resources/views/partials/_profile-nav.blade.php
@@ -1,29 +1,45 @@
 
 <nav aria-hidden="true" class="menu menu-right" id="profile" tabindex="-1">
 	<div class="menu-scroll">
-		<div class="menu-top">
-			<div class="menu-top-img">
-				<img alt="John Smith" src="../images/samples/landscape.jpg">
+
+		@if (Auth::check())
+			<div class="menu-top">
+				<div class="menu-top-img">
+					<img alt="{{ Auth::user()->name }}" src="../images/samples/landscape.jpg">
+				</div>
+				<div class="menu-top-info">
+					<a class="menu-top-user" href="javascript:void(0)"><span class="avatar pull-left"><img alt="{{ Auth::user()->name }}" src="../images/users/avatar-001.jpg"></span>{{ Auth::user()->name }}</a>
+				</div>
+				<div class="menu-top-info-sub">
+					<small>Some additional information about John Smith</small>
+				</div>
 			</div>
-			<div class="menu-top-info">
-				<a class="menu-top-user" href="javascript:void(0)"><span class="avatar pull-left"><img alt="alt text for John Smith avatar" src="../images/users/avatar-001.jpg"></span>John Smith</a>
+
+			<div class="menu-content">
+				<ul class="nav">
+					<li>
+						<a class="waves-attach" href="javascript:void(0)"><span class="icon icon-lg">account_box</span>Profile Settings</a>
+					</li>
+					<li>
+						<a class="waves-attach" href="/auth/logout"><span class="icon icon-lg">exit_to_app</span>Logout</a>
+					</li>
+				</ul>
 			</div>
-			<div class="menu-top-info-sub">
-				<small>Some additional information about John Smith</small>
+
+		@else
+
+			<div class="menu-content">
+				<ul class="nav">
+					<li>
+						<a class="waves-attach" href="/auth/login"><span class="icon icon-lg">account_box</span>Login</a>
+					</li>
+					<li>
+						<a class="waves-attach" href="/auth/register"><span class="icon icon-lg">add_to_photos</span>Register</a>
+					</li>
+				</ul>
 			</div>
-		</div>
-		<div class="menu-content">
-			<ul class="nav">
-				<li>
-					<a class="waves-attach" href="javascript:void(0)"><span class="icon icon-lg">account_box</span>Profile Settings</a>
-				</li>
-				<li>
-					<a class="waves-attach" href="javascript:void(0)"><span class="icon icon-lg">add_to_photos</span>Upload Photo</a>
-				</li>
-				<li>
-					<a class="waves-attach" href="page-login.html"><span class="icon icon-lg">exit_to_app</span>Logout</a>
-				</li>
-			</ul>
-		</div>
+
+		@endif
+
 	</div>
 </nav>
