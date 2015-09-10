6ea799dc95b22519e6029d69642e5340bb214118
gbelot2003 <gbelot2003@hotmail.com>
2015-09-10 13:28:03 -0600

    agregando clases de bootstrap a _flash.blade

--- resources/views/partials/_flash.blade.php
+++ resources/views/partials/_flash.blade.php
@@ -1,13 +1,12 @@
 @if(Session::has('flash_message'))
-	<div id="flash_success" class="card-panel #1565c0 blue darken-3">
-		<button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
+	<div class="alert alert-success alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 		<span class="white-text"><i class="smaller material-icons">done</i>{{Session('flash_message')}}</span>
 	</div>
 @elseif(Session::has('errors'))
-	<div id="flash_error" class="card-panel #c62828 red darken-3">
-		<button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
-		<span class="white-text"><i class="smaller material-icons">error</i><strong>Whoops!</strong> Hay algunos problemas con su entrada de datos!!!.</span>
-		<br><br>
+	<div class="alert alert-warning alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<strong>Warning!</strong>
 		<ol>
 			@foreach ($errors->all() as $error)
 				<li><span class="white-text">{{ $error }}</span></li>
@@ -15,3 +14,4 @@
 		</ol>
 	</div>
 @endif
+
