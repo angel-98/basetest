2c2a19d1f4d63e7737bef72cee1495175f9afa9b
gbelot2003 <gbelot2003@hotmail.com>
2015-09-11 22:52:21 -0600

    roles.index agregando braeadcrumbs y partial de modal

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layout.app')
+
 @section('page-title', 'Administración de Roles')
+
 @section('v-control', 'id="roles"')
+
+@section('breadcrumb')
+	<li><a href="/">Dashboard</a></li>
+	<li><a class="active" href="/roles">Roles</a></li>
+@stop
 
 @section('content')
 	<div class="row">
@@ -29,56 +36,10 @@
 
 	@include('partials._vue-data')
 
-
 @stop
 
 @section('modal')
-	<div aria-hidden="true" class="modal fade" id="modal-create" role="dialog" tabindex="-1" style="display: none;">
-		<form method="post" v-on="submit: submitRoles">
-			<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-heading">
-							<a class="modal-close" data-dismiss="modal">×</a>
-							<h2 class="modal-title">Modal Title</h2>
-						</div>
-						<div class="modal-inner">
-							<div class="row">
-								<div class="col-sm-12 col-md-4">
-									<div class="form-group form-group-label">
-
-										<div class="row">
-
-											<div class="col-sm-12">
-												<label class="floating-label" for="name">Nombre</label>
-												<input class="form-control" maxlength="65" id="name" type="text" name="email" v-model="roles.name">
-											</div>
-										</div>
-									</div>
-								</div>
-
-								<div class="col-sm-12 col-md-8">
-									<div class="form-group form-group-label">
-										<div class="row">
-											<div class="col-sm-12">
-												<label class="floating-label" for="description">Descripción</label>
-												<input class="form-control" maxlength="255" id="description" type="text" name="description" v-model="roles.description">
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-
-
-						<div class="modal-footer">
-							<p class="text-right">
-								<button type="submit"  class="btn btn-blue">Create</button>
-								<button class="btn btn-flat btn-alt"  data-dismiss="modal" type="button">Close</button></p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</form>
-	</div>
+	@include('roles.create._modal-create')
 @stop
 
 @section('post-script')
