a8260a9ca2576c313303bfa065046545cb0b0f5c
gbelot2003 <gbelot2003@hotmail.com>
2015-09-11 22:19:10 -0600

    implementando modal para la eliminación de roles

---
+++ resources/views/roles/edit/_modal-delete.blade.php
@@ -0,0 +1,24 @@
+<div aria-hidden="true" class="modal fade" id="modal-delete" role="dialog" tabindex="-1" style="display: none;">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-heading">
+					<a class="modal-close" data-dismiss="modal">×</a>
+					<h2 class="modal-title">Eliminar rol</h2>
+				</div>
+				<div class="modal-inner">
+					<h4>Estas seguro de quere eliminar el rol <strong class="text-red">{{ $roles->name }}</strong>, esta operacion no se puede revertir</h4>
+				</div>
+					<div class="modal-footer">
+						<p class="text-right">
+						
+						<form action="{{ action('RolesController@destroy', $roles->id) }}" method="post">
+							{!! csrf_field() !!}
+							<input name="_method" type="hidden" value="delete">
+								<button class="btn btn-red" type="submit" >Eliminar</button>
+						</form>
+						<button class="btn btn-flat btn-alt"  data-dismiss="modal" type="button">Cancelar</button></p>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
