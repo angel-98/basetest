1c8ab4f6a654c97d005ee576dee43e7cf50b0dd8
gbelot2003 <gbelot2003@hotmail.com>
2015-09-13 15:16:15 -0600

    Vista de usuario sin autorización al sistema

---
+++ resources/views/dash/unauthorize.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('content')
+	<h3 class="text-red">No tienes autorización para ingresar al sistema</h3>
+	<p>Se a creado tu perfil, pero este aun no tiene autorización o permisos para ingresar, deberas a esperar a que el administrador del sistema te de los permisos correspondientes</p>
+	<p>Si crees que este mensaje es un error, verifica tus credenciales y/o comunicate con el administrador del sistema</p>
+@stop
