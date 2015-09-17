4d9f2927674c65928b0f5b7b5644d61eb7c1c263
gbelot2003 <gbelot2003@hotmail.com>
2015-09-17 12:22:27 -0600

    corrigiendo markup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,33 +6,53 @@
 	<div class="row">
 		<div class="col-lg-6 col-lg-push-3 col-sm-10 col-sm-push-1 ">
 			<!-- resources/views/auth/register.blade.php -->
-
 			<form method="POST" action="/auth/register">
 				{!! csrf_field() !!}
+				<fieldset>
+					<legend>Registrar</legend>
 
-				<div>
-					Name
-					<input type="text" name="name" value="{{ old('name') }}">
-				</div>
+					<div class="form-group form-group-label">
+						<div class="row">
+							<div class="col-lg-12 col-sm-8">
+								<label class="floating-label" for="name">Nombre</label>
+								<input class="form-control" id="name" type="text" name="name">
+							</div>
+						</div>
+					</div>
 
-				<div>
-					Email
-					<input type="email" name="email" value="{{ old('email') }}">
-				</div>
 
-				<div>
-					Password
-					<input type="password" name="password">
-				</div>
+					<div class="form-group form-group-label">
+						<div class="row">
+							<div class="col-lg-12 col-sm-8">
+								<label class="floating-label" for="email">E-mail</label>
+								<input class="form-control" id="email" type="email" name="email">
+							</div>
+						</div>
+					</div>
 
-				<div>
-					Confirm Password
-					<input type="password" name="password_confirmation">
-				</div>
+					<div class="form-group form-group-label">
+						<div class="row">
+							<div class="col-lg-12 col-sm-8">
+								<label class="floating-label" for="password">Password</label>
+								<input class="form-control" id="password" type="password" name="password" >
+							</div>
+						</div>
+					</div>
 
-				<div>
-					<button type="submit">Register</button>
-				</div>
+					<div class="form-group form-group-label">
+						<div class="row">
+							<div class="col-lg-12 col-sm-8">
+								<label class="floating-label" for="password">Password de confirmación</label>
+								<input class="form-control" id="password" type="password" name="password_confirmation" >
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-xx-6">
+							<p class="margin-top-no"><button class="btn btn-blue" type="submit">Registrar</button></p>
+						</div>
+					</div>
+				</fieldset>
 			</form>
 		</div>
 	</div>
