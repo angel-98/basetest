fba0d54db2228ddeb3496e70fcff90f834bd9a75
gbelot2003 <gbelot2003@hotmail.com>
2015-09-23 08:42:04 -0600

    actualizando marco de la vista auth.password

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,14 +1,26 @@
-<form method="POST" action="/password/email">
+@extends('layout.app')
+@section('page-title', 'Password Reset')
+
+@section('content')
+
+	<form method="POST" action="/password/email">
 	{!! csrf_field() !!}
 
-	<div>
-		Email
-		<input type="email" name="email" value="{{ old('email') }}">
-	</div>
+		<div class="form-group form-group-label">
+			<div class="row">
+				<div class="col-lg-12 col-sm-8">
+					<label class="floating-label" for="email">E-mail</label>
+					<input class="form-control" id="email" type="email" name="email" value="{{ old('email') }}">
+				</div>
+			</div>
+		</div>
 
 	<div>
-		<button type="submit">
-			Send Password Reset Link
-		</button>
+		<div class="form-group">
+			<div class="col-xx-6">
+				<p class="margin-top-no"><button class="btn btn-blue" type="submit">Send Password Reset Link</button></p>
+			</div>
+		</div>
 	</div>
 </form>
+@stop
