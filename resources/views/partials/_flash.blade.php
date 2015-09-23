53c8d196343db95c6383fab2d97534d6a80c8c61
gbelot2003 <gbelot2003@hotmail.com>
2015-09-22 22:48:54 -0600

    corrigiendo markup

--- resources/views/partials/_flash.blade.php
+++ resources/views/partials/_flash.blade.php
@@ -1,11 +1,14 @@
 @if(Session::has('flash_message'))
+
 	<div class="alert alert-success alert-dismissible" role="alert">
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<span class="icon text-alt">error</span>
 		<span class="white-text"><i class="smaller material-icons">done</i>{{Session('flash_message')}}</span>
 	</div>
+
 @elseif(Session::has('errors'))
+
 	<div class="alert alert-warning alert-dismissible" role="alert">
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<span class="icon text-alt">error</span>
 		<strong>Warning!</strong>
 		<ol>
 			@foreach ($errors->all() as $error)
@@ -13,5 +16,6 @@
 			@endforeach
 		</ol>
 	</div>
+
 @endif
 
