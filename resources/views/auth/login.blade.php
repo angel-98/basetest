2a29a7495268c2e0c051bf5a2f14d123aded16f9
gbelot2003 <gbelot2003@hotmail.com>
2015-09-24 12:10:41 -0600

    creando y agregando enlace a pagias estaticas y de ayuda

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,42 +3,70 @@
 <!-- resources/views/auth/login.blade.php -->
 @section('content')
 	<div class="row">
-		<div class="col-lg-6 col-lg-push-3 col-sm-10 col-sm-push-1 ">
-			<form method="POST" action="/auth/login">
-				{!! csrf_field() !!}
-				<fieldset>
-					<div class="form-group form-group-label">
-						<div class="row">
-							<div class="col-lg-12 col-sm-8">
-								<label class="floating-label" for="email">E-mail</label>
-								<input class="form-control" id="email" type="email" name="email" value="{{ old('email') }}">
+		<div class="col-lg-4 col-lg-push-4 col-sm-6 col-sm-push-3">
+			<section class="content-inner">
+				<div class="card-wrap">
+					<div class="card">
+						<div class="card-main">
+							<div class="card-header">
+								<div class="card-inner">
+									<h1 class="card-heading">Login</h1>
+								</div>
+							</div>
+							<div class="card-inner">
+								<p class="text-center">
+												<span class="avatar avatar-inline avatar-lg">
+													<img alt="Login" src="../images/users/avatar-001.jpg">
+												</span>
+								</p>
+								<form method="POST" action="/auth/login">
+									{!! csrf_field() !!}
+									<div class="form-group form-group-label">
+										<div class="row">
+											<div class="col-md-10 col-md-push-1">
+												<label class="floating-label" for="email">E-mail</label>
+												<input class="form-control" id="email" type="email" name="email" value="{{ old('email') }}">
+											</div>
+										</div>
+									</div>
+									<div class="form-group form-group-label">
+										<div class="row">
+											<div class="col-md-10 col-md-push-1">
+												<label class="floating-label" for="login-password">Password</label>
+												<input class="form-control" id="password" type="password" name="password" >
+											</div>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<div class="row">
+											<div class="col-md-10 col-md-push-1">
+												<button class="btn btn-block waves-attach waves-button">Login</button>
+											</div>
+										</div>
+									</div>
+									<div class="form-group">
+										<div class="row">
+											<div class="col-md-10 col-md-push-1">
+												<div class="checkbox checkbox-adv">
+													<label for="input-checkbox-1">
+														<input class="access-hide" id="input-checkbox-1" name="remember" type="checkbox">Recordarme
+														<span class="circle"></span><span class="circle-check"></span><span class="circle-icon icon">done</span>
+													</label>
+												</div>
+											</div>
+										</div>
+									</div>
+								</form>
 							</div>
 						</div>
 					</div>
-
-					<div class="form-group form-group-label">
-						<div class="row">
-							<div class="col-lg-12 col-sm-8">
-								<label class="floating-label" for="password">Password</label>
-								<input class="form-control" id="password" type="password" name="password" >
-							</div>
-						</div>
-					</div>
-
-					<div class="checkbox checkbox-adv">
-						<label for="input-checkbox-1">
-							<input class="access-hide" id="input-checkbox-1" name="remember" type="checkbox">Remember Me
-							<span class="circle"></span><span class="circle-check"></span><span class="circle-icon icon">done</span>
-						</label>
-					</div>
-					<hr/>
-					<div class="form-group">
-						<div class="col-xx-6">
-							<p class="margin-top-no"><button class="btn btn-blue" type="submit">Login</button></p>
-						</div>
-					</div>
-				</fieldset>
-			</form>
+				</div>
+				<div class="clearfix">
+					<p class="margin-no-top pull-left"><a class="btn btn-flat btn-blue waves-attach" href="/help">¿Necesita ayuda?</a></p>
+					<p class="margin-no-top pull-right"><a class="btn btn-flat btn-blue waves-attach" href="/auth/register">Crear Cuenta</a></p>
+				</div>
+			</section>
 		</div>
 	</div>
 @stop
---
+++ resources/views/pages/help.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.app')
+
+@section('page-title', 'Administración de Usuarios')
+
+@section('breadcrumb')
+	<li><a href="/">Dashboard</a></li>
+	<li><a class="active" href="/help">Ayuda</a></li>
+@stop
